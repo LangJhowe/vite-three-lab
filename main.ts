63c2d598a60712ffea4235b804a14ec5fc7475c1
leung jhowe <ail>
2021-03-03 23:46:51 +0800

    :tada:tada: init vite-vanilla-ts.

---
+++ main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+let dom = document.querySelector('#app')
+if(dom){
+  dom.innerHTML = `
+    <h1>Hello Vite!</h1>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  `
+}
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+const pathResolve = (pathStr: string) => {
+  return path.resolve(__dirname, pathStr)
+}
+
+export default defineConfig({})
